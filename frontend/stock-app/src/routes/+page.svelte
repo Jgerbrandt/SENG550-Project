8e0d27ff7eb472c86915efdb50bad3e5ea779e9e
Ethan Subasic <ethan.subasic@gmail.com>
2024-12-19 07:33:11 -0700

    added static chart

--- frontend/stock-app/src/routes/+page.svelte
+++ frontend/stock-app/src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import PocketBase from 'pocketbase';
+    import { Chart, registerables } from 'chart.js';
 
     const pb = new PocketBase('http://127.0.0.1:8090');
+    Chart.register(...registerables);
 
-    let item: { ml: any, actual: any } = { ml: null, actual: null };
+    let item: { ml: any[], actual: any[] } = { ml: [], actual: [] };
 
-    // Fetch initial data
+	let chart_context: any;
+	let chart_canvas: any;
+
+    // Fetch initial data from the collection
     async function fetchData() {
         try {
             const result = await pb.collection('AMZN').getList(1, 1, {});
@@ -37,7 +42,23 @@
         }).catch((error: any) => {
             console.error('Error subscribing to collection:', error);
         });
+
+		chart_context = chart_canvas.getContext('2d');
+            var chart = new Chart(chart_context, {
+				type: 'line',
+				data: {
+						labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+						datasets: [{
+								label: 'Revenue',
+								backgroundColor: 'rgb(255, 99, 132)',
+								borderColor: 'rgb(255, 99, 132)',
+								data: [0, 10, 5, 2, 20, 30, 45],
+						}]
+				}
+		});
+
     });
+
 </script>
 
 <main>
@@ -46,4 +67,7 @@
         <div>ML: {item.ml}</div>
         <div>Actual: {item.actual}</div>
     </div>
+	
+	<canvas bind:this={chart_canvas} id="myChart"></canvas>
+
 </main>
